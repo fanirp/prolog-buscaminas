eb1524f69e2bf0beffa5cb2fd1d10a6d831bb5fe
marianobianchi08 <marianobianchi08@95b11c38-d14f-c2bb-cf04-774ace2b2603>
2011-06-03 14:49:55 +0000

    Perdon, era hasta el ejercicio 3. Saqué una cosa que habia quedado colgada en el archivo...

--- trunk/codigo/ejercicios.pl
+++ trunk/codigo/ejercicios.pl
@@ -2,15 +2,6 @@
 entre(X,Y,X) :- X=<Y.
 entre(X,Y,Z) :- X<Y,N is X+1,entre(N,Y,Z).
 
-%esPar(+X)
-esPar(0).
-esPar(X) :- X>0,N is X-2,esPar(N).
-
-%pares(+X,-Z)
-pares(X,Z) :- esPar(X), Z is X.
-pares(X,Z) :- esPar(X), Y is X+2, pares(Y,Z).
-
-
 %valor(-X)
 valor(mina).
 valor(N) :- entre(0,8,N).
